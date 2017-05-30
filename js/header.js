7d09529ac0772273d109132d30ac0b2dd9c1d487
djk <dkats12@gmail.com>
2017-05-29 20:06:23 -0400

    Restructured; wrote 'about' pages; made global header and footer

---
+++ js/header.js
@@ -0,0 +1,13 @@
+document.write('<header>');
+document.write('<ul class="menu">');
+document.write('	<li class="noclick">Dr. DANce</li>');
+document.write('	<li onclick="window.open(\'index.html\');">How can we exercise?</li>');
+document.write('	<li onclick="window.open(\'why.html\');">Why should we exercise?</li>');
+document.write('	<li onclick="window.open(\'how-much.html\');">How much should we exercise?</li>');
+document.write('	<!-- <li style="float:right;" onclick="window.open(\'https://github.com/dkats/DrDANce\');">Source code</li> -->');
+document.write('	<!-- <li style="float:right;" onclick="window.open(\'mailto:djk175@case.edu?subject=Dr. DANce bug! Ew!\');">Report bug</li> -->');
+document.write('	<!-- <li style="float:right;" onclick="window.open(\'mailto:djk175@case.edu?subject=Dr. DANce question\');">Contact</li> -->');
+document.write('	<li style="float:right;" onclick="window.open(\'references.html\');">References</li>');
+document.write('	<li style="float:right;" onclick="window.open(\'about.html\');">About</li>');
+document.write('</ul>');
+document.write('</header>');
